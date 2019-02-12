52e5a601905a021e0e57977df0b1711b761fa515
ltrii <lelandtr@gmail.com>
2019-02-12 12:46:49 -0800

    PUT

--- posts/post-router.js
+++ posts/post-router.js
@@ -62,5 +62,22 @@ router.delete('/:id', async (req, res) => {
       });
     }
   });
+
+  router.put('/:id', async (req, res) => {
+    try {
+      const post = await db.update(req.params.id, req.body);
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: 'The post could not be found' });
+      }
+    } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the post',
+      });
+    }
+  });
  
  module.exports = router;
