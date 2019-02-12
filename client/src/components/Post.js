592c46508eb102a9d236d2dc88e7dc116e657f98
ltrii <lelandtr@gmail.com>
2019-02-12 13:15:49 -0800

    Stretch done but needs styling

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 export default class Post extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: this.props.post.title,
+            contents: this.props.post.contents
+        }
+    }
   render() {
     return (
-      <div>
-        Hello
+      <div className="aPost">
+        <h2>
+            {this.state.title}
+        </h2>
+        <p>
+            {this.state.contents}
+        </p>
       </div>
     )
   }
