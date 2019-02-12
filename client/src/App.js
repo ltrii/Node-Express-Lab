fc66d12069d6a626fb9768d98243f873352f93bd
ltrii <lelandtr@gmail.com>
2019-02-12 13:07:40 -0800

    More stretch work

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PostList from './components/PostList';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
+            <PostList />
         </header>
       </div>
     );
--- client/src/components/postList.js
+++ client/src/components/postList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import Axios from "axios";
+import PostMap from './PostMap';
 
-export default class postList extends Component {
+export default class PostList extends Component {
     constructor(){
         super();
+        this.state = {
+          initialized: false,
+          posts: [],
+        }
     }
 
+    componentDidMount(){
+      Axios
+      .get(
+          'http://localhost:4000/api/posts'
+      )
+      .then(res => {
+        this.setState({
+          posts: res.data
+        })
+      } )
+      .catch(err => console.log('error fetching'))
+    }
 
   render() {
     return (
