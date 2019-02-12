29d6259af8aebce327e17f72d5d98f50a5d40afc
ltrii <lelandtr@gmail.com>
2019-02-12 12:31:13 -0800

    Index work

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express'); // importing a CommonJS module
+
+const postsRouter = require('./posts/post-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', hubsRouter);
+
+server.get('/', async (req, res) => {
+  res.send(`
+    <h2>Posts</h>
+    <p>Get some posts</p>
+  `);
+});
+
+module.exports = server;
