5904ef5fd9684e96d023c74c5b747e598562eb44
ltrii <lelandtr@gmail.com>
2019-02-14 11:19:52 -0800

    Work for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,19 @@ import PostList from './components/PostList';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.connectToServer = this.connectToServer.bind(this);
+  }
+
+  connectToServer() {
+    fetch('/');
+  }
+
+  componentDidMount() {
+    this.connectToServer();
+  }
+
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -3,7 +3,26 @@ const cors = require('cors');
 
 const postsRouter = require('./posts/post-router');
 
+const path = require('path');
+const port = process.env.PORT || 4000;
+
 const server = express();
+
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 
 server.use(express.json());
 
