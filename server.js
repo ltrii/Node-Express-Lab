5df9a606bee90312e9939b8d2a511ec7593c34b0
ltrii <lelandtr@gmail.com>
2019-02-14 11:47:07 -0800

    Small fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(express.static(path.join(__dirname, 'client/build')))
 if(process.env.NODE_ENV === 'production') {
   server.use(express.static(path.join(__dirname, 'client/build')));
   //
-  app.get('*', (req, res) => {
+  server.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 }
